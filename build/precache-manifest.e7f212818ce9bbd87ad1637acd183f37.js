1a05233f276f028c794ae42c3cfc3ce86969bdeb
bard.bringeland@gmail.com <bard.bringeland@gmail.com>
2020-07-12 21:12:41 +0200

    Adding production build

---
+++ build/precache-manifest.e7f212818ce9bbd87ad1637acd183f37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a8475014bfb9e42f5970470404bee34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f59cdbe7713efb3e82d",
+    "url": "/static/css/main.f4a976e0.chunk.css"
+  },
+  {
+    "revision": "e85f2d4c65b83362d0c1",
+    "url": "/static/js/2.02e359b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.02e359b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f59cdbe7713efb3e82d",
+    "url": "/static/js/main.38665688.chunk.js"
+  },
+  {
+    "revision": "e9c987836f4015f310d2",
+    "url": "/static/js/runtime-main.fda2ac98.js"
+  }
+]);
